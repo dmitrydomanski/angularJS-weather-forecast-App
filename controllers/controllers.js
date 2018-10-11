55843741dc212e9e7e3fff4f10224bfadc16786d
Dmitry Domanski <dmitryd@codeoasis.com>
2018-10-11 16:32:00 +0300

    weatherService implemented

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,39 +1,36 @@
-
-weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
-    $scope.city = cityService.city;
-
-    $scope.$watch('city', function () {
-        cityService.city = $scope.city;
-    })
-}]);
-
-weatherApp.controller('forecastController', ['$scope', '$http', '$routeParams', 'cityService',
-    function ($scope, $http, $routeParams, cityService) {
-
-        $scope.weatherResult = ''
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService',
+    function ($scope, $location, cityService) {
         $scope.city = cityService.city;
-        $scope.entries = $routeParams.entries || 5;
-        $scope.apiKey = "b5d2be91d9878b08d0193bd934b0740d";
 
-        $scope.getDay = function(day) {
+        $scope.$watch('city', function () {
+            cityService.city = $scope.city;
+        });
+
+        $scope.submit = function () {
+
+            $location.path("/forecast");
+
+        };
+    }
+]);
+
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService',
+    function ($scope, $routeParams, cityService, weatherService) {
+
+        $scope.city = cityService.city;
+        $scope.apiKey = "b5d2be91d9878b08d0193bd934b0740d";
+        $scope.entries = $routeParams.entries || 5;
+        
+        weatherService.getWeather($scope.city, $scope.apiKey, $scope.entries).then(function (response) {
+            $scope.weatherResult = response.data;
+        });;
+
+        $scope.getDay = function (day) {
             return day * 1000;
         }
 
-        $scope.getTemp = function(temp) {
+        $scope.getTemp = function (temp) {
             return (temp - 273.15).toFixed()
         }
-
-        $http.get("http://api.openweathermap.org/data/2.5/forecast", {
-                params: {
-                    appid: $scope.apiKey,
-                    q: $scope.city.replace(', ', ','),
-                    cnt: $scope.entries
-                }
-            })
-            .then(function (response) {
-                $scope.weatherResult = response.data;
-            });
     }
-
-
 ]);
--- services/city-service.js
+++ services/city-service.js
@@ -1,4 +1,18 @@
 // SERVICE
 weatherApp.service("cityService", function () {
     this.city = 'Tel Aviv, IL';
-})
+});
+
+weatherApp.service("weatherService", ['$http', function ($http) {
+
+    this.getWeather = function (city, apiKey, entries) {
+       return $http.get("http://api.openweathermap.org/data/2.5/forecast", {
+                params: {
+                    appid: apiKey,
+                    q: city.replace(', ', ','),
+                    cnt: entries
+                }
+            });
+    }
+
+}]);
